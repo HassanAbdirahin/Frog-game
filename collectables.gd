b5b2fc2ad851ff53d8cbb107144b24e772496fc8
HassanAbdirahin <hassanabdirahin441@gmail.com>
2025-02-09 11:59:12 +0300

    randomized collectables

--- collectables.gd
+++ collectables.gd
@@ -5,6 +5,8 @@ var coins = preload("res://coins/coins.tscn");
 
 func _on_timer_timeout():
 	var coinsTemp = coins.instantiate();
-	coinsTemp.global_position = Vector2(128, 384)
+	var rng = RandomNumberGenerator.new();
+	var randInt = rng.randi_range(96, 2960)
+	coinsTemp.global_position = Vector2(randInt, 384)
 	add_child(coinsTemp);
 	
